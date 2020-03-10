f41563c6d45b86c6752dd9d70bd4120a6a92a976
Criska <619729921@qq.com>
2020-03-11 01:03:34 +0800

    按钮

---
+++ components/z-listbar/z-listbar.js
@@ -0,0 +1,44 @@
+// components/z-listbar/z-listbar.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    list:[
+      {
+        name:'每日推荐',
+        icon:'/images/tuijian1.png',
+        classname:"tuijian"
+      },
+      {
+        name: '歌单',
+        icon: '/images/gedan.png',
+        classname: "gedan"
+      },
+      {
+        name: '排行榜',
+        icon: '/images/paihang1.png',
+        classname: "paihang"
+      },
+      {
+        name: '电台',
+        icon: '/images/diantai.png',
+        classname: "diantai"
+      }
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- components/z-swiper/z-swiper.js
+++ components/z-swiper/z-swiper.js
@@ -1,5 +1,5 @@
 // components/z-swiper/z-swiper.js
-import API from '/utils/Api/api.js'
+import API from '../..//utils/Api/api.js'
 Component({
   /**
    * 组件的属性列表
@@ -15,6 +15,7 @@ Component({
     indicatorDots: true,
     autoplay: true,
     background: ['red', 'yellwo', 'black'],
+    banners:null
 
   },
   /**
@@ -22,10 +23,11 @@ Component({
    */
   lifetimes: {
     created: function () {
-      
+      this.getBannerData()
     },
     attached: function () {
       // 在组件实例进入页面节点树时执行
+      //this.getBannerData()
     },
     detached: function () {
       // 在组件实例被从页面节点树移除时执行
@@ -36,6 +38,14 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+      //获取轮播图数据
+      getBannerData(){
+        API.getBanner({type:1}).then((res)=>{
+          this.setData({
+            banners: res.banners
+          })
+          console.log(res.banners)
+        })
+      }
   }
 })
