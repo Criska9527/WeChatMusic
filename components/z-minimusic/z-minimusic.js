90a878a8a96e6c745d5ae59d84e609edcbbc2585
Criska <619729921@qq.com>
2020-12-20 22:01:18 +0800

    111

--- components/z-minimusic/z-minimusic.js
+++ components/z-minimusic/z-minimusic.js
@@ -1,4 +1,6 @@
 // components/z-minimusic/z-minimusic.js
+//获取应用实例
+const app = getApp()
 Component({
   /**
    * 组件的属性列表
@@ -18,20 +20,35 @@ Component({
    * 组件的方法列表
    */
   methods: {
-    music(){
+    music() {
+      
+      //判断当前是否有歌曲被播放
+      if (!app.globalData.musicinfo) {
+        wx.lin.showToast({
+          title: '暂无歌曲播放',
+          icon: 'error',
+          success: (res) => {
+            console.log(res)
+          },
+          complete: (res) => {
+            console.log(res)
+          }
+        })
+        return
+      }
       wx.navigateTo({
         url: '/pages/music/music',
         events: {
           // 为指定事件添加一个监听器，获取被打开页面传送到当前页面的数据
-          acceptDataFromOpenedPage: function (data) {
+          acceptDataFromOpenedPage: function(data) {
             console.log(data)
           },
-          someEvent: function (data) {
+          someEvent: function(data) {
             console.log(data)
           }
 
         },
-        success: function (res) {
+        success: function(res) {
           // 通过eventChannel向被打开页面传送数据
           res.eventChannel.emit('acceptDataFromOpenerPage', {
             data: 'test'
--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -8,22 +8,30 @@ Component({
     color: "#8D8D8D",
     selectedColor: "#8D8D8D",
     list: [{
-      pagePath: "/pages/find/find",
-      iconPath: "/images/music.png",
-      selectedIconPath: "/images/selected-music.png",
-      text: "乐库"
-    },
-    {
-      pagePath: "/pages/mine/mine",
-      iconPath: "/images/me.png",
-      selectedIconPath: "/images/selected-me.png",
-      text: "我的"
-    }]
+        pagePath: "/pages/find/find",
+        iconPath: "/images/music.png",
+        selectedIconPath: "/images/selected-music.png",
+        text: "乐库"
+      },
+      {
+        pagePath: "/pages/search/search",
+        iconPath: "/images/搜索.png",
+        selectedIconPath: "/images/selected-music.png",
+        text: "搜索"
+      },
+
+      {
+        pagePath: "/pages/mine/mine",
+        iconPath: "/images/me.png",
+        selectedIconPath: "/images/selected-me.png",
+        text: "我的"
+      }
+    ]
   },
 
   methods: {
 
-    switchTab_index: function () {
+    switchTab_index: function() {
       // this.setData({
       //   isShow_index: true,
       //   isShow_me: false,
@@ -32,10 +40,10 @@ Component({
       wx.switchTab({
         url: '/pages/find/find'
       })
- 
+
     },
 
-    switchTab_playing: function () {
+    switchTab_playing: function() {
       // this.setData({
       //   isShow_playing: true,
       //   isShow_index: false,
@@ -44,10 +52,10 @@ Component({
       wx.switchTab({
         url: '/pages/friend/friend'
       })
-    
+
     },
 
-    switchTab_me: function () {
+    switchTab_me: function() {
       // this.setData({
       //   isShow_me: true,
       //   isShow_playing: false,
@@ -56,7 +64,18 @@ Component({
       wx.switchTab({
         url: '/pages/mine/mine'
       })
- 
+
+    },
+    switchTab_search:function () {
+      // this.setData({
+      //   isShow_me: true,
+      //   isShow_playing: false,
+      //   isShow_index: false
+      // })
+      wx.switchTab({
+        url: '/pages/search/search'
+      })
+
     }
   }
 })
